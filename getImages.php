dafec8a8287b808ec91a83dff938c3c33a8174ea
davidcopano <davidcopano96@gmail.com>
2017-02-14 11:00:43 +0100

    Primer commit pal juego de los coj****

---
+++ getImages.php
@@ -0,0 +1,78 @@
+<?php
+function shuffle_assoc($array) {
+    $keys = array_keys($array);
+
+    shuffle($keys);
+
+    $new = "";
+
+    foreach($keys as $key) {
+        $new[$key] = $array[$key];
+    }
+
+    $array = $new;
+
+    return true;
+}
+
+if(isset($_GET["d"]))
+    {
+        $archivos = scandir("images/cartas");
+
+        // Necesario para que no muestre '.' y '..'
+
+        array_shift($archivos);
+        array_shift($archivos);
+
+        shuffle($archivos);
+
+        $dificultad = intval($_GET["d"]);
+
+        switch ($dificultad)
+        {
+            // Tamaño de 12 cartas (6 cartas con su pareja)
+            case 1: {
+                for($i = 0; $i < 14; $i++) {
+                    array_pop($archivos);
+                }
+            }
+
+            break;
+
+            // Tamaño de 16 cartas (8 cartas con su pareja)
+            case 2: {
+                for($i = 0; $i < 12; $i++) {
+                    array_pop($archivos);
+                }
+            }
+
+            break;
+
+            case 3: {
+                for($i = 0; $i < 10; $i++) {
+                    array_pop($archivos);
+                }
+            }
+
+                break;
+        }
+
+        // Conversión a JSON
+
+        $salida = "{\"imagenes\": [";
+
+        for($i = 0; $i < count($archivos); $i++) {
+            $salida .= "{ \"id\": " . ($i+1) . ", \"url\": " . "\"images/cartas/" . $archivos[$i] . "\"},";
+        }
+
+        $salida .= "]}";
+
+        $salida = substr($salida, 0, -3);
+
+        $salida .= "]}";
+
+        echo($salida);
+    }
+    else {
+        die("No tiene permiso para visualizar esta página.");
+    }
